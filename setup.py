16d6aa31f104f2584a1fbdb64237540ab97ffa31
Leonardo Taccari <leonardo.taccari@gmail.com>
2017-10-19 19:52:28 +0200

    Revert latest commit that checked in WIP code. Fixes #7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='joypy',
-      version='0.1.4',
+      version='0.1.5',
       description='Joyplots in python',
       long_description='Joyplots in python.',
       classifiers=[
