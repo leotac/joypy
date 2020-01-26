c1d9c224ca08c456ab76a428418415914b68b7c2
Leonardo Taccari <floor51@gmail.com>
2020-01-26 11:14:39 +0100

    Bump version: 0.2.1 → 0.2.2

--- joypy/__init__.py
+++ joypy/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'Leonardo Taccari'
 __email__ = 'leonardo.taccari@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 from .joyplot import joyplot, plot_density, _joyplot
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='joypy',
-      version='0.2.1',
+      version='0.2.2',
       description='Joyplots in python',
       long_description='Joyplots in python.',
       classifiers=[
