edf82741491ee9c0e5a6cc9bfc8b1f36278360a3
Leonardo Taccari <leonardo.taccari@gmail.com>
2020-12-28 12:53:41 +0100

    Update build and publish script

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+rm -rf dist
 bumpversion patch #major minor patch - bump version in all files
 python setup.py sdist bdist_wheel --universal
 twine upload dist/**tar.gz dist/**whl
